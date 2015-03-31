d24f34f28c96ba7930e90f5da229197474e64ec5
Blake Bourque <Techplex.Engineer@gmail.com>
2015-03-30 20:06:43 -0400

    Scaffolded app

---
+++ src/app/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('resume', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'app/about/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
