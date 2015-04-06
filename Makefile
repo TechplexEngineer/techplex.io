f1fea2fffa1e3a02b7abecc53098b35e37cbd28d
Blake Bourque <Techplex.Engineer@gmail.com>
2015-04-06 00:17:22 -0400

    Adding the rating system, not sure I love it

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+all:
+  @echo "deploy   Sends the dist directory to techplex.io"
+
+deploy:
+  rsync -v -r  ./dist/ techplex@blake.metheus.org:/var/www/techplex.io/
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,63 +2,128 @@
 
 angular.module('resume')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
+    var ctrl = this;
+    var data = {
+      name: {
+        first:"Blake",
+        last:"Bourque"
       },
-      {
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'title': 'jQuery',
-        'url': 'http://jquery.com/',
-        'description': 'jQuery is a fast, small, and feature-rich JavaScript library.',
-        'logo': 'jquery.jpg'
-      },
-      {
-        'title': 'Bootstrap',
-        'url': 'http://getbootstrap.com/',
-        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        'logo': 'bootstrap.png'
-      },
-      {
-        'title': 'Less',
-        'url': 'http://lesscss.org/',
-        'description': 'Less extends the CSS language, adding features that allow variables, mixins, functions and many other techniques.',
-        'logo': 'less.png'
+      career: "Computer Engineer",
+      link: "techplex.io",
+      mail: "blake@techplex.io",
+      github: "TechplexEngineer",
+      lcol: [
+        {
+          title: "Professional Experience",
+          items: [
+            {
+              title:"Summer Electrical Engineering Co-Op",
+              company:"Sappi Fine Paper North America",
+              description: [
+                "Managed the final electrical connection and closeout of three new fire protection systems.",
+                "Designed wiring diagrams using AutoCAD for Kajaani ash meter replacement, and managed project.",
+                "Developed a test procedure for the finishing wrapline HMI virtualization project."
+              ],
+              start:"May 2014",
+              end:"Aug 2014",
+            },{
+              title:"Summer Information Technology Co-Op",
+              company:"Sappi Fine Paper North America",
+              description: [
+                "Developed a web database application to track critical network-connected devices in ASP.Net MVC.",
+                "Collaborated with process control and network engineers to refine user experience of the application."
+              ],
+              start:"May 2013",
+              end:"Aug 2013",
+            },{
+              title:"Summer Programming Intern",
+              company:"Kepware Technologies",
+              description: [
+                "Programmed a module in C++ which allows KepServerEX to access remote data via XML SOAP.",
+                "Developed documentation for internal and customer use."
+
+              ],
+              start:"May 2012",
+              end:"Aug 2012",
+            },{
+              title:"Web Application Engineer",
+              company:"DeepCwind Consortium",
+              description: [
+                "Designed an online purchase order reimbursement system, with dynamic totaling and validation.",
+                "Implemented a remote data acquisition system to act as a scoring system for the Windstorm Challenge."
+              ],
+              start:"Feb 2012",
+              end:"Apr 2012",
+            },{
+              title:"Founder/Web Application Engineer",
+              company:"Techplex Labs",
+              description: [
+                "Designed and built websites, for a variety of organizations on strict budgets.",
+                "Responsible for the development and maintenance of web database applications."
+              ],
+              start:"Jan 2009",
+              end:"Sep 2012",
+            },{
+              title:"Robotics Counselor",
+              company:"Maine Robotics",
+              description: [
+                "Encouraged youth to learn more about engineering and computer science with Lego Mindstorms system.",
+                "Provided a supportive environment for students to be excited about science and technology."
+              ],
+              start:"Jul & Aug 2008",
+              end:"2011",
+            }
+          ]
+        },{
+          title: "Education",
+          items: [
+            {
+              title:"Bachelor of Science Computer Engineering",
+              company:"University of Maine",
+              description: [
+                "Expected May 2015",
+                "GPA 3.717"
+              ],
+              start:"May 2014",
+              end:"Aug 2014",
+            },
+          ]
+        }
+
+
+
+      ],
+      rcol: {
+        summary: "",
+        ratings: [
+          {
+            title:"Technical Skills",
+            items: []
+          }
+        ],
+        lists: [
+          {
+            title:"Honors",
+            type: "awards",
+            items: [
+              "Eagle Scout, Troop 453",
+              "Vice President Eta Kappa Nu Honor Society"
+            ]
+          },{
+            title:"Scholarships / Involvement",
+            type: "awards scholarship",
+            items: [
+              "University of Maine Pulp & Paper Foundation Scholarship 2011-2015",
+              "Mentor FIRST Robotics Team 5122, The RoboTies",
+              "IEEE Member"
+            ]
+          }
+        ]
       }
-    ];
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
-    });
+
+    };
+
+    ctrl = _.merge(ctrl, data);
+    // console.log(ctrl);
   });
+
